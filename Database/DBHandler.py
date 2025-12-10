777c1c3b779c0306c775a740509e7db9c0fae612
Mateusz Musial <mateuszmusial95@gamil.com>
2025-12-10 19:16:45 +0100

    <AWCR_System> Database schema and DBHandler refactor
    * Added SQL file with database schema
    * Refactored DBHandler.py

--- Database/DBHandler.py
+++ Database/DBHandler.py
@@ -10,15 +10,15 @@ logger = get_logger()
 class DBHandler:
     def __init__(self):
         self.db_name = "awcr_database"
-        self.conn = sqlite3.connect(self.db_name)
-        self.cursor = self.conn.cursor()
 
     def user_login(self, user_email: str, user_password: str) -> bool:
         """
         Check if user is in the database and password is correct.
         """
-        self.cursor.execute("SELECT password FROM Users WHERE email = (?) ", (user_email, ))
-        result = self.cursor.fetchone()
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT password FROM Users WHERE email = (?) ", (user_email, ))
+            result = cursor.fetchone()
 
         if result:
             return check_password(user_password, result[0])
@@ -31,89 +31,61 @@ class DBHandler:
         hashed_password = hash_password(password)
 
         try:
-            self.cursor.execute("INSERT INTO Users (email, password) VALUES (?, ?)",
+            with sqlite3.connect(self.db_name) as connection:
+                cursor = connection.cursor()
+                cursor.execute("INSERT INTO Users (email, password) VALUES (?, ?)",
                                 (email, hashed_password))
-            self.conn.commit()
-            logger.info(f"User {email} successfully registered to AWCR System!")
+                connection.commit()
+                logger.info(f"User {email} successfully registered to AWCR System!")
         except sqlite3.IntegrityError:
             logger.error("User already exist in the AWCR System!")
             return False, "User already exist in the AWCR System!"
 
         return True, f"User {email} successfully registered to AWCR System!"
 
-    def close_connection(self):
-        self.conn.close()
-
     def fetch_detections_data(self, period: str) -> list[tuple]:
-        """
-        Fetch data from the database based on the specified period.
-        """
+        """Fetch data based on period."""
+        now = datetime.now()
+
         match period:
             case "Today":
-                days_ago = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
-                self.cursor.execute("SELECT * FROM Detections WHERE timestamp >= ?", (days_ago,))
+                days_ago = now - timedelta(days=1)
             case "Last week":
-                days_ago = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d %H:%M:%S')
-                self.cursor.execute("SELECT * FROM Detections WHERE timestamp >= ?", (days_ago,))
+                days_ago = now - timedelta(days=7)
             case "Last month":
-                days_ago = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d %H:%M:%S')
-                self.cursor.execute("SELECT * FROM Detections WHERE timestamp >= ?", (days_ago,))
+                days_ago = now - timedelta(days=30)
             case "Last year":
-                days_ago = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d %H:%M:%S')
-                self.cursor.execute("SELECT * FROM Detections WHERE timestamp >= ?", (days_ago,))
+                days_ago = now - timedelta(days=365)
             case _:
                 logger.error("Unrecognized time period!")
                 return []
 
-        data = self.cursor.fetchall()
-        return data
+        timestamp_str = days_ago.strftime('%Y-%m-%d %H:%M:%S')
 
-    def check_detected_car_in_database(self, final_result):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Detections WHERE timestamp >= ?", (timestamp_str,))
+            return cursor.fetchall()
+
+    def check_detected_car_in_database(self, final_result: str) -> tuple[bool, tuple | None]:
         """
         Check if the detected car is already in the database.
         """
-        self.cursor.execute("SELECT * FROM Cars WHERE license_plate = (?)", (final_result,))
-        result = self.cursor.fetchone()
-        if result:
-            return True, result
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Cars WHERE license_plate = (?)", (final_result,))
+            result = cursor.fetchone()
+            if result:
+                return True, result
         return False, None
 
-    def add_detection(self, licence_plate):
+    def add_detection(self, licence_plate: str) -> None:
         """
         Add detection to the database.
         """
-        self.cursor.execute("INSERT INTO Detections (license_plate, timestamp) VALUES (?, datetime('now'))",
-                            (licence_plate,))
-        self.conn.commit()
-
-    '''
-    Script to create database structure.
-    '''
-    # def setup_database(self):
-    #     self.cursor.execute('''
-    #     CREATE TABLE IF NOT EXISTS Users (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         email TEXT NOT NULL UNIQUE,
-    #         password TEXT NOT NULL
-    #     );
-    #     ''')
-    #
-    #     self.cursor.execute('''
-    #     CREATE TABLE IF NOT EXISTS Cars (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         license_plate TEXT NOT NULL UNIQUE,
-    #         brand TEXT,
-    #         model TEXT,
-    #         vin_number TEXT NOT NULL UNIQUE
-    #         );
-    #     ''')
-    #
-    #     self.cursor.execute('''
-    #     CREATE TABLE IF NOT EXISTS Detections (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         license_plate TEXT NOT NULL,
-    #         timestamp TEXT NOT NULL,
-    #         car_id INTEGER,
-    #         FOREIGN KEY (car_id) REFERENCES Cars(id)
-    #     );
-    #     ''')
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO Detections (license_plate, timestamp) VALUES (?, datetime('now'))",
+                           (licence_plate,))
+            connection.commit()
+            logger.info(f"Added detection of car with licence plate {licence_plate} to the database.")
