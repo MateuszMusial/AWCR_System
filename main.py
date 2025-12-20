033977268b4766857d048a3515ae1bedf159766b
Mateusz Musial <mateuszmusial95@gamil.com>
2025-12-20 13:15:34 +0100

    <AWCR_System> Added main
    * Added main function
    * Updated YOLO model

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import logging
+
+
+from logger import setup_logger
+from GUI.app import GuiHandler
+
 
 def main():
-    pass
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    logging.getLogger('PIL').setLevel(logging.WARNING)
+    setup_logger()
+
+    gui_handler = GuiHandler()
+    gui_handler.create_window("Login / Register")
+    gui_handler.setup_login_register_window()
+    gui_handler.window.mainloop()
 
 
 if __name__ == '__main__':
     main()
-
